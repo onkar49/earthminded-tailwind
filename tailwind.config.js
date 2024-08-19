c685a3d632ebc16f760147ae1149d1263724b763
onkar49 <63900543+onkar49@users.noreply.github.com>
2024-08-19 13:56:15 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  keyframes: {
+    "infinite-scroll": {
+      "0%": {transform: "translateX(0)"},
+      "100%": {transform: "translateX(calc(-100%))"},
+    },
+  },
+  animation: {
+    "infinite-scroll": "infinite-scroll 40s linear infinite",
+  },
+  plugins: [],
+}
+
